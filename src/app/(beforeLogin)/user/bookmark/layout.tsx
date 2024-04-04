d999713274680daccd48d312c05c479a315b864e
byunghyun <eer3481@naver.com>
2024-04-04 12:58:22 +0900

    feat: bookmark page 구성

---
+++ src/app/(beforeLogin)/user/bookmark/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import AptModalPopupContainer from '@/app/_components/common/CarsayoModalContainer';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const BookmarkModalLayout = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  const router = useRouter();
+  const handleClickEvents = {
+    goBackHistory: () => {
+      router.back();
+    },
+  };
+  return (
+    <AptModalPopupContainer
+      isShown={true}
+      onClose={handleClickEvents.goBackHistory}
+    >
+      {children}
+    </AptModalPopupContainer>
+  );
+};
+
+export default BookmarkModalLayout;
